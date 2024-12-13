d8d682d31045813a42db1443fef1f2f6236741f5
Dmitry Kuzmin <dskuzmin@edu.hse.ru>
2024-12-13 12:37:52 +0300

    upd: unify traits methods

--- src/traits.rs
+++ src/traits.rs
@@ -1,22 +1,19 @@
-use crate::staff::{manager::Manager, worker::Worker};
+use crate::types::Staff;
 use crate::types::{Resource, Result};
-use std::cell::RefCell;
-use std::collections::HashSet;
-use std::rc::Rc;
 use uuid::Uuid;
 
 /// [`StaffEntity`] provides basic methods for company staff entity
 pub trait StaffEntity {
     fn get_id(&self) -> Uuid;
 
-    fn get_resource_amount(&self) -> Resource;
+    fn get_resource_amount(&self) -> Result<Resource>;
 
     /// Method returns an amount of spended resources or [`crate::errors::StaffError::InsufficientResourcesError`]
     fn spend(&mut self, amount: Resource) -> Result<Resource>;
 
     fn send_resource(&mut self, to: Uuid, amount: Resource) -> Result<Resource>;
 
-    fn recieve_resource(&mut self, amount: Resource) -> Result<()>;
+    fn recieve_resource(&mut self, amount: Resource) -> Result<Resource>;
 }
 
 /// [`Employee`]
@@ -26,12 +23,7 @@ pub trait Employee {
 
 /// [`Supervisor`]
 pub trait Supervisor {
-    fn hire_employee(&mut self, employee: Rc<RefCell<Worker>>) -> Result<Rc<RefCell<Worker>>>;
-    fn hire_manager(&mut self, manager: Rc<RefCell<Manager>>) -> Result<Rc<RefCell<Manager>>>;
+    fn hire(&mut self, staff_entity: Staff) -> Result<Uuid>;
 
-    fn layoff_employee(&mut self, employee_id: Uuid) -> Result<Rc<RefCell<Worker>>>;
-    fn layoff_manager(&mut self, manager_id: Uuid) -> Result<Rc<RefCell<Manager>>>;
-
-    fn assume_subordinates(&mut self, subordinates: HashSet<Uuid>);
-    fn release_subordinates(&mut self) -> HashSet<Uuid>;
+    fn layoff(&mut self, staff_id: Uuid) -> Result<Staff>;
 }

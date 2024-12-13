0df6790532043fe36c26a5ab45e94f64619702bf
Dmitry Kuzmin <dskuzmin@edu.hse.ru>
2024-12-13 17:54:51 +0300

    add: tests for resource transferring

--- tests/interaction_tests.rs
+++ tests/interaction_tests.rs
@@ -33,42 +33,4 @@ mod tests {
         ceo.send_resource(director.get_id(), 100).unwrap();
         assert_eq!(100, director.get_resource_amount().unwrap());
     }
-
-    #[test]
-    fn test_layoff_workers_balances() {
-        let company = Rc::new(RefCell::new(Company::Google(Google::new())));
-
-        let mut ceo = CEO::new(company.clone());
-        company
-            .as_ref()
-            .borrow_mut()
-            .set_ceo(Staff::Ceo(ceo.clone()))
-            .unwrap();
-        company
-            .borrow_mut()
-            .recieve_resource(ceo.get_id(), 10000)
-            .unwrap();
-
-        let mut workers: Vec<Uuid> = vec![];
-        for _ in 0..5 {
-            let worker = Worker::new(company.clone());
-            let worker_id = worker.get_id();
-            workers.push(worker.get_id());
-
-            ceo.hire(Staff::Employee(worker)).unwrap();
-            ceo.send_resource(worker_id, 100).unwrap();
-        }
-
-        assert_eq!(9500, ceo.get_resource_amount().unwrap());
-
-        for id in workers.clone() {
-            assert_eq!(100, company.borrow().get_resource_amount(id).unwrap());
-        }
-
-        for id in workers {
-            ceo.layoff(id).unwrap();
-        }
-
-        assert_eq!(10000, ceo.get_resource_amount().unwrap());
-    }
 }

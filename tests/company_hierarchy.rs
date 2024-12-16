b53ec6b1d1238424fc3f59c2d2356f7ec78a5818
Dmitry Kuzmin <dskuzmin@edu.hse.ru>
2024-12-16 13:24:46 +0300

    add: test for change supervisor

--- tests/company_hierarchy.rs
+++ tests/company_hierarchy.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod tests {
-    use staff_lib::{Company, StaffEntity};
+    use staff_lib::{errors::Error, Company, StaffEntity};
     use uuid::Uuid;
 
     #[derive(Debug)]
@@ -55,5 +55,48 @@ mod tests {
         }
 
         assert_eq!(*company.resource(&ceo_id).unwrap(), mint_amount);
+    }
+
+    #[test]
+    fn test_change_supervisor() {
+        let ceo = Manager::new();
+        let ceo_id = ceo.get_id();
+
+        let mut company: Company<Manager, u64> = Company::new(ceo);
+
+        let first_manager_id = company.hire(Manager::new(), &ceo_id).unwrap();
+        let second_manager_id = company.hire(Manager::new(), &first_manager_id).unwrap();
+
+        if let Err(Error::HierarchyConflict {
+            staff_id,
+            supervisor_id,
+        }) = company.change_supervisor(&first_manager_id, &second_manager_id)
+        {
+            assert_eq!(staff_id, first_manager_id);
+            assert_eq!(supervisor_id, second_manager_id);
+        } else {
+            panic!("Expected HierarchyConflict error");
+        }
+
+        let third_manager_id = company.hire(Manager::new(), &second_manager_id).unwrap();
+
+        if let Err(Error::HierarchyConflict {
+            staff_id,
+            supervisor_id,
+        }) = company.change_supervisor(&first_manager_id, &third_manager_id)
+        {
+            assert_eq!(staff_id, first_manager_id);
+            assert_eq!(supervisor_id, third_manager_id);
+        } else {
+            panic!("Expected HierarchyConflict error");
+        }
+
+        // Успешное изменение супервизора
+        assert!(company
+            .change_supervisor(&third_manager_id, &ceo_id)
+            .is_ok());
+
+        // Проверка общего количества сотрудников
+        assert_eq!(company.get_all_staff().len(), 4); // CEO + 3 managers
     }
 }
